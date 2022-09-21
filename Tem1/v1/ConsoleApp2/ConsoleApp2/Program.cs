173c1b2407ae1cb703f04770a24daa515cc9eb9d
Resonat228 <82326701+Resonat228@users.noreply.github.com>
2022-09-21 14:41:18 +0300

    Add files via upload

---
+++ Tem1/v1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,156 @@
+﻿namespace ConsoleApplication1
+{
+    public static class CustomExtensions
+    {
+        private static readonly Dictionary<string, Actions> ActionsMap = new()
+        {
+            { "+", Actions.SimpleAdd },
+            { "*", Actions.PreferredAdd },
+            { "-", Actions.Remove },
+        };
+
+        public static (Actions, string) SplitOnTwo(this string str, char separator)
+        {
+            var firstValue = string.Empty;
+            var scndValue = string.Empty;
+
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (str[i] == separator)
+                {
+                    if (i != str.Length - 1)
+                    {
+                        for (var j = i + 1; j < str.Length; j++)
+                        {
+                            scndValue += str[j];
+                        }
+                    }
+                    return (firstValue.ConvertToEnum(), scndValue);
+                }
+
+                firstValue += str[i];
+            }
+
+            return (firstValue.ConvertToEnum(), scndValue);
+        }
+
+
+        private static Actions ConvertToEnum(this string action)
+        {
+            if (ActionsMap.ContainsKey(action))
+            {
+                return ActionsMap[action];
+            }
+
+            return Actions.Default;
+        }
+    }
+
+    internal class GoblinQueue
+    {
+        private readonly List<string> _currentQueue;
+
+        public GoblinQueue()
+        {
+            _currentQueue = new List<string>();
+        }
+
+        public void Add(string name, Actions actions)
+        {
+            int index;
+            switch (actions)
+            {
+                case Actions.PreferredAdd:
+                    index = _currentQueue.Count / 2;
+                    break;
+                case Actions.SimpleAdd:
+                    index = _currentQueue.Count;
+                    break;
+                default:
+                    throw new ArgumentException();
+            }
+
+            _currentQueue.Insert(index, name);
+
+        }
+
+        public string Remove()
+        {
+            if (_currentQueue.Count > 0)
+            {
+                var removeElement = _currentQueue.First();
+                _currentQueue.Remove(removeElement);
+                return removeElement;
+            }
+
+            return string.Empty;
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var stringCountAction = Console.ReadLine();
+
+            if (!int.TryParse(stringCountAction, out var countActions))
+            {
+                Console.WriteLine("Invalid count");
+                return;
+            }
+
+            var goblinQueue = new GoblinQueue();
+            var removedGoblins = new List<string>();
+
+            for (var i = 0; i < countActions; i++)
+            {
+                var goblin = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(goblin))
+                {
+                    Console.WriteLine("Invalid action check input");
+                    return;
+                }
+
+                var (action, goblinName) = goblin.SplitOnTwo(' ');
+
+                if (action == Actions.Default)
+                {
+                    Console.WriteLine("Invalid action check input");
+                    return;
+                }
+
+                if (action == Actions.Remove)
+                {
+                    removedGoblins.Add(goblinQueue.Remove());
+                    continue;
+                }
+
+                if (goblin != string.Empty)
+                {
+                    goblinQueue.Add(goblinName, action);
+                }
+            }
+
+            PrintRemovedGoblins(removedGoblins);
+
+        }
+
+        private static void PrintRemovedGoblins(List<string> removedGoblins)
+        {
+            foreach (var goblin in removedGoblins)
+            {
+                Console.Write(goblin + "\n");
+            }
+        }
+    }
+
+    public enum Actions
+    {
+        SimpleAdd = 0,
+        PreferredAdd = 1,
+        Remove = 2,
+
+        Default = -1,
+    }
+}
---
+++ Tem1/v3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split();
+            var listdata = new List<int>(input.Select(int.Parse));
+            
+            var inputMoments = Console.ReadLine().Split();
+            var queueOfMoments = new List<int>(inputMoments.Select(int.Parse));
+
+            var maximumAmount = 0;
+
+            for (int i = 0; i < listdata[0] - listdata[1]; i++)
+            {
+                var temporaryAmount = queueOfMoments[i] + queueOfMoments.Skip(i + listdata[1]).Max();
+                
+                if (maximumAmount < temporaryAmount)
+                {
+                    maximumAmount = temporaryAmount;
+                }
+            }
+            
+            Console.WriteLine(maximumAmount);
+        }
+    }
+
+}
+
